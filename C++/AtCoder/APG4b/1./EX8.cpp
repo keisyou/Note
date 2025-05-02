a3281c1ee83e9d9a0d9db0a62317bcf3fce5f182
keisyou <shtk0llq@gmail.com>
2025-05-03 01:35:27 +0900

    20250503 #1

---
+++ C++/AtCoder/APG4b/1./EX8.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int p, price, N;
+  cin >> p;
+
+  // パターン1
+  if (p == 1) {
+    cin >> price >> N;
+  }
+
+  // パターン2
+  if (p == 2) {
+    string text;
+    cin >> text >> price >> N;
+    cout << text << "!" << endl;
+  }
+
+  cout << price * N << endl;
+}
